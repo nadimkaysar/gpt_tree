ecb20128036854da8e9dcf98f336fbf685da8e1e
Kaysar Md Nadim <39157267+nadimkaysar@users.noreply.github.com>
2025-10-01 18:54:38 +0900

    Update and rename DialogPrompt.py to SystemPrompt.py

---
+++ Source/SystemPrompt.py
@@ -0,0 +1,8 @@
+def counseling_prompt():
+  prompt = f""" Context: You are a psychologist and your specialization in well-being support for patient symtomps reduction. You can chat with Japanese and English language.
+       
+       Task instruction: Try to understand patient situation context. Then, you need to counseling patient based on patient situation.
+
+       You have to generate your respone within 40 words
+  """
+  return prompt
