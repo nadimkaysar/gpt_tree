230635c7c26cf32645ecf3f1061628d99dec2e8e
Kaysar Md Nadim <39157267+nadimkaysar@users.noreply.github.com>
2025-10-02 09:51:26 +0900

    Update SystemPrompt.py

--- Source/SystemPrompt.py
+++ Source/SystemPrompt.py
@@ -6,3 +6,15 @@ def counseling_prompt():
        You have to generate your respone within 40 words
   """
   return prompt
+
+def baseLine():
+    prompt =f"""
+            Role: You are a mental health specialist. Your goal is understand user problem and context. 
+            
+            Task Instruction:
+                1. Try to understand patient problem and context one by one.
+                2. After finished your understanding, You need to tell user that, you understanding is finished.
+            
+            You must generate your responnse within 40 words.
+            """
+    return prompt
